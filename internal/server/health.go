f59e84251ebc374a314f45e49fa6bae457df8eb1
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-09-08 15:26:11 +0300

    feat: add configuration loading and update health check response headers

--- internal/server/health.go
+++ internal/server/health.go
@@ -3,11 +3,15 @@ package server
 import "net/http"
 
 func Ping(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("PONG"))
 }
 
 func HealthProxy(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status":"ok"}`))
 }
